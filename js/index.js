24cc89be68a08f5a58e8822c6690912a2cefce54
dnjswlekd <wj8014@gmail.com>
2023-06-16 18:19:54 +0900

    피드백 50% 수정완료 (myLife 추가예정)

--- js/index.js
+++ js/index.js
@@ -19,3 +19,31 @@ window.addEventListener("scroll", function () {
     targetElement.classList.remove("active");
   }
 });
+
+// fortfolio
+
+document.addEventListener("DOMContentLoaded", function () {
+  var list = document.querySelectorAll("#portfolio>.container>.mnu>li>a");
+  var listImg = document.querySelectorAll("#portfolio>.container>.view>li");
+  var nowIdx = 0;
+
+  list.forEach(function (item, index) {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      nowIdx = index;
+
+      item.parentNode.classList.add("on");
+      var siblings = Array.from(item.parentNode.parentNode.children).filter(function (child) {
+        return child !== item.parentNode;
+      });
+      siblings.forEach(function (sibling) {
+        sibling.classList.remove("on");
+      });
+
+      listImg.forEach(function (img) {
+        img.style.display = "none";
+      });
+      listImg[nowIdx].style.display = "block";
+    });
+  });
+});
