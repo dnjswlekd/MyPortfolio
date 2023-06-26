e1e0b1230fb54afbc9cbee5e135aa120d40160a3
dnjswlekd <wj8014@gmail.com>
2023-06-26 16:04:59 +0900

    포폴 최종 수정
    하루동안 보지않기 모달 수정중

--- js/index.js
+++ js/index.js
@@ -55,3 +55,68 @@ window.onload = function () {
     modal.style.display = "none";
   });
 };
+
+//modal 하루동안 보지않기
+// window.addEventListener("DOMContentLoaded", function () {
+//   var popupBox = document.querySelector(".popup_box");
+//   popupBox.addEventListener("mousedown", dragPopup);
+
+//   var popupLayer = document.getElementById("popup_layer");
+//   if (document.cookie.indexOf("popToday=close") < 0) {
+//     popupLayer.style.display = "block";
+//   } else {
+//     popupLayer.style.display = "none";
+//   }
+// });
+
+// function dragPopup(event) {
+//   event.preventDefault();
+//   var offsetX = event.clientX - event.target.offsetLeft;
+//   var offsetY = event.clientY - event.target.offsetTop;
+
+//   document.addEventListener("mousemove", movePopup);
+//   document.addEventListener("mouseup", stopDrag);
+
+//   function movePopup(event) {
+//     var x = event.clientX - offsetX;
+//     var y = event.clientY - offsetY;
+
+//     var parentWidth = event.target.parentElement.clientWidth;
+//     var parentHeight = event.target.parentElement.clientHeight;
+
+//     var maxX = parentWidth - event.target.offsetWidth;
+//     var maxY = parentHeight - event.target.offsetHeight;
+
+//     x = Math.max(0, Math.min(x, maxX));
+//     y = Math.max(0, Math.min(y, maxY));
+
+//     event.target.style.left = x + "px";
+//     event.target.style.top = y + "px";
+//   }
+
+//   function stopDrag() {
+//     document.removeEventListener("mousemove", movePopup);
+//     document.removeEventListener("mouseup", stopDrag);
+//   }
+// }
+
+// function closeToday() {
+//   setCookie("popToday", "close", 1);
+//   var popupLayer = document.getElementById("popup_layer");
+//   popupLayer.style.display = "none";
+// }
+
+// function closePop() {
+//   var popupLayer = document.getElementById("popup_layer");
+//   popupLayer.style.display = "none";
+// }
+
+// function setCookie(name, value, days) {
+//   var expires = "";
+//   if (days) {
+//     var date = new Date();
+//     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+//     expires = "; expires=" + date.toUTCString();
+//   }
+//   document.cookie = name + "=" + (value || "") + expires + "; path=/";
+// }
